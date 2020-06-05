c06f20ac6f74905afcf9910bd4eb93e4626672b8
Andre Emidio <andre@datatype.com.br>
2020-06-04 23:40:12 -0300

    Correção da rota de processamento de imagem

--- apps/imagens/api/viewsets.py
+++ apps/imagens/api/viewsets.py
@@ -1,5 +1,22 @@
+import base64
+import io
+
+import cv2
 from apps.imagens.models import Imagens
-from rest_framework import viewsets
+from cv2.cv2 import imread
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+try:
+    from StringIO import StringIO ## for Python 2
+except ImportError:
+    from io import StringIO ## for Python 3
+import numpy as np
+from django.http import JsonResponse
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
 from .serializers import ImagensSerializer
 
 
@@ -7,3 +24,19 @@ class ImagensViewSet(viewsets.ModelViewSet):
     serializer_class = ImagensSerializer
     queryset = Imagens.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        imagem = serializer.data.get('imagem')
+
+        sbuf = BytesIO()
+        sbuf.write(base64.b64decode(imagem))
+        pimg = Image.open(sbuf)
+
+        cv2_img = cv2.cvtColor(np.array(pimg), cv2.COLOR_BGR2GRAY)
+        # cv2.imwrite("reconstructed.jpg", cv2_img)
+        image_encode = cv2.imencode('.jpg', cv2_img)[1]
+        image_encode = base64.b64encode(image_encode).decode('utf8')
+
+        return Response({"imagem_cinza": image_encode}, status=status.HTTP_200_OK)
